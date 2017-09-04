fc5c97da90bc41d0de440f9b4b3244a20e901fa1
Andres Mena <ballerlikemike23@gmail.com>
2017-09-04 19:25:00 +0000

    Creating Campgrounds

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://www.rei.com/adventures/assets/adventures/images/trip/gallery/northamerica/fzc_04"},
+  {name: "Granite Hill", image: "http://www.thingsyouneedforcamping.com/wp-content/uploads/2016/11/camping.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"}
+];
 
 app.get("/", function(req, res){
   res.render("landing");
 })
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-      {name: "Salmon Creek", image: "https://www.rei.com/adventures/assets/adventures/images/trip/gallery/northamerica/fzc_04"},
-      {name: "Granite Hill", image: "http://www.thingsyouneedforcamping.com/wp-content/uploads/2016/11/camping.jpg"},
-      {name: "Mountain Goat's Rest", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"}
-    ]
-    
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name; 
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
